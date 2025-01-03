08170cea74b14d47c0b9de74370f3c3595a843f4
jhajhan <jhannasllovit@iskolarngbayan.pup.edu.ph>
2025-01-03 15:54:03 +0800

    db

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+
+$host = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
+
+// ?>
---
+++ config/tempCodeRunnerFile.php
@@ -0,0 +1,8 @@
+<?php
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}

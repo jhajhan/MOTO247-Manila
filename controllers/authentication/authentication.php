714aa61984678ad2edc95cdf46febc5657b9d8e6
jhajhan <jhannasllovit@iskolarngbayan.pup.edu.ph>
2025-01-25 15:44:56 +0800

    nagana na

--- controllers/authentication/authentication.php
+++ controllers/authentication/authentication.php
@@ -3,6 +3,12 @@
     require_once __DIR__ . '/../../config/db.php';
     require_once __DIR__ . '/../../session/session_manager.php';
     require_once __DIR__ . '/../../session/auth_session.php';
+    require __DIR__ . '/../../vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+
 
 
     class Authentication {
@@ -25,7 +31,7 @@
             $stmt->bind_param('sssssss', $email, $username, $password, $full_name, $role, $created_at, $token);
             
             if ($stmt->execute()) {
-             //   $this->sendVerificationEmail($email, $token);
+              $this->sendVerificationEmail($email, $token);
                 echo json_encode(['message' => 'User registered successfully']);
             } else {
                 echo json_encode(['message' => 'Failed to register user']);
@@ -37,9 +43,33 @@
 
         function sendVerificationEmail ($email, $token) {
             // using smtp
-            
+            $mail = new PHPMailer(true);
 
+            try {
+                //Server settings
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'rjhoncarlos30@gmail.com';
+                $mail->Password = 'wedr geug bmcz qrie';
+                $mail->SMTPSecure = 'tls';
+                $mail->Port = 587;
 
+                //Recipients
+                $mail->setFrom('moto247manila@gmail.com');
+                $mail->addAddress($email);
+                
+
+                // Content
+                $mail->isHTML(true);
+                $mail->Subject = 'Account Verification';
+                $mail->Body = "Click <a href='http://localhost:8000/verify-email?token=$token'>here</a> to verify your account.";
+
+                $mail->send();
+                echo 'Verification email sent!';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
 
         }
 
@@ -47,15 +77,21 @@
 
             global $conn;
 
-            $email = htmlspecialchars($data['email']);
+
+            $username = htmlspecialchars($data['username']);
             $password = $data['password'];
 
-            $query = "SELECT * FROM user WHERE email = ?";
+            
+
+            $query = "SELECT * FROM user WHERE username = ?";
             $stmt = $conn->prepare($query);
-            $stmt->bind_param('s', $email);
-            $stmt->execute();
+            $stmt->bind_param('s', $username);
+            echo $stmt->execute();
             $result = $stmt->get_result();
             $user = $result->fetch_assoc();
+
+            echo json_encode($user);
+            echo password_verify($password, $user['password']);
 
         
             if ($user) {
@@ -74,22 +110,26 @@
 
         function verifyEmail ($token) {
 
+            echo 'Yow';
+
             global $conn;
 
-            $query = "SELECT id FROM users WHERE verification_token = ?";
+            $query = "SELECT user_id FROM user WHERE token = ?";
             $stmt = $conn->prepare($query);
             $stmt->bind_param("s", $token);
-            $stmt->execute();
+            echo $stmt->execute();
             $result = $stmt->get_result();
 
             if ($result->num_rows > 0) {
+
                 // Update the user's verification status
-                $updateQuery = "UPDATE users SET is_verified = 1, verification_token = NULL WHERE verification_token = ?";
+                $updateQuery = "UPDATE user SET verified = 1, token = NULL WHERE token = ?";
                 $updateStmt = $conn->prepare($updateQuery);
                 $updateStmt->bind_param("s", $token);
                 $updateStmt->execute();
 
                 echo "Email verified! You can now log in.";
+                header('/login');
             } else {
                 echo "Invalid or expired token.";
             }
